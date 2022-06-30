f0bf5cdf5e2935c403cde4656bf10d240c68a0d1
quyetpham <quyetpham>
2022-06-30 16:51:07 +0700

    init

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const URL = process.env.MONGO_URL;
 
 const app = express();
-const PORT = 5000 || 3000;
+// const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
@@ -31,8 +31,8 @@ app.use('/api/v1/user', users);
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("connect done");
-        app.listen(5000, () => {
-            console.log(`server is runing on port 5000`);
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is runing on port 8000`);
         });
     }).catch(err => {
         console.log('err', err);
