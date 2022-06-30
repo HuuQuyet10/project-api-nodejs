09d7380cf6a1281a1b3db2eb6d5a5580e618a4e1
quyetpham <quyetpham>
2022-06-30 16:43:04 +0700

    update

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use('/api/v1/user', users);
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("connect done");
-        app.listen(PORT, () => {
-            console.log(`server is runing on port ${PORT}`);
+        app.listen(5000, () => {
+            console.log(`server is runing on port 5000`);
         });
     }).catch(err => {
         console.log('err', err);
